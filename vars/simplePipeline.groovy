ca7a1abac47c3a0ce001c9b0263830e4cef0716c
satishgummadelli <satishgummadelli@gmail.com>
2019-07-15 14:29:23 +0100

    test

--- vars/simplePipeline.groovy
+++ vars/simplePipeline.groovy
@@ -10,7 +10,7 @@ import org.apache.kafka.clients.admin.KafkaAdminClient
 import org.apache.kafka.clients.admin.NewTopic
 
 def kerb() {
-    System.setProperty("java.security.auth.login.config", "/usr/share/jenkins/ref/jaas1.conf")
+    System.setProperty("java.security.auth.login.config", "/var/jenkins_home/jaas1.conf")
     props = new HashMap<String, Object>([(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG): 'kafka-3-node-0.ujgcetklw1vu1ail22tmlbzzad.zx.internal.cloudapp.net:9092,kafka-3-node-1.ujgcetklw1vu1ail22tmlbzzad.zx.internal.cloudapp.net:9092,kafka-3-node-2.ujgcetklw1vu1ail22tmlbzzad.zx.internal.cloudapp.net:9092', (AdminClientConfig.CLIENT_ID_CONFIG): UUID.randomUUID().toString()])
 
     props.put("security.protocol", "SASL_SSL");
